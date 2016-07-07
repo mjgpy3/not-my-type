f5edee5b2c79c8a7875573a269ecd1b1691db05b
Michael Gilliland <mjg.py3@gmail.com>
2016-07-06 20:00:58 -0400

    Add Either and some methods

--- index.js
+++ index.js
@@ -10,5 +10,55 @@ function curryN(n, fn, args) {
     }
 };
 
+function Left(value) {
+  this.map = function (_) {
+    return new Left(value);
+  };
+
+  this.equals = function (other) {
+    return other.isLeft && value == other.fromLeft();
+  };
+
+  this.either = function (left, _) {
+    return left(value);
+  };
+
+  this.fromLeft = function () {
+    return value;
+  };
+
+  this.fromRight = function () {
+    throw 'fromRight called on a Left';
+  };
+
+  this.isLeft = true;
+  this.isRight = false;
+}
+
+function Right(value) {
+  this.map = function (fn) {
+    return new Right(fn(value));
+  };
+
+  this.equals = function (other) {
+    return other.isRight && value == other.fromRight();
+  };
+
+  this.either = function (_, right) {
+    return right(value);
+  };
+
+  this.fromRight = function () {
+    return value;
+  };
+
+  this.fromLeft = function () {
+    throw 'fromLeft called on a Right';
+  };
+
+  this.isLeft = false;
+  this.isRight = true;
+}
+
 module.exports = {
 };
